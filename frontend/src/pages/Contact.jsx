1c0d509db4ab5549040f6e3922c506ad87dffcf6
ShiwankAks <aksshiwank@gmail.com>
2025-09-11 16:20:39 +0530

    Removed the admin reference in verify file

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -19,9 +19,9 @@ const Contact = () => {
             <br/>Greater Noida,Uttar Pradesh 201310
           </p>
           <p className=' text-gray-500'>Tel: (415) 555-0132
-            <br/>Email: admin@now.com
+            <br/>Email: admin@forever.com
           </p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Now</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>
             Explore Jobs
--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -2,12 +2,11 @@ import React, { useContext, useEffect } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { useSearchParams } from 'react-router-dom'
 import axios from 'axios'
-import { backendUrl } from '../../../admin/src/App'
 import {toast} from 'react-toastify'
 
 const Verify = () => {
 
-    const {navigate, token , setCartItems} = useContext(ShopContext)
+    const {navigate, token , setCartItems,backendUrl} = useContext(ShopContext)
     const [searchParams, setSearchParams] = useSearchParams()
 
     const success = searchParams.get("success")
