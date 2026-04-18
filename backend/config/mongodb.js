5048f0c0c06f5453c276b347ee663a68b418751e
ShiwankAks <aksshiwank@gmail.com>
2026-04-18 14:18:15 +0530

    env updated

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -7,6 +7,11 @@ const connectDB = async () => {
     });
 
     await mongoose.connect(`${process.env.MONGODB_URI}`);
+    // console.log("DB URL Length:", process.env.MONGODB_URI?.length);
+    // console.log(
+    //   "DB URL Starts With:",
+    //   process.env.MONGODB_URI?.substring(0, 15),
+    // );
   } catch (error) {
     console.log("Error while connecting to mongoDB : " + error);
   }
